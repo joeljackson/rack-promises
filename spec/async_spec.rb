287ad2f667d816ca7d03a84612ccde9b631b55e6
Joel Jackson <jackson.joel@gmail.com>
2013-06-05 09:05:19 -0700

    test for error being raised if pcall is not defined

--- spec/async_spec.rb
+++ spec/async_spec.rb
@@ -1,4 +1,5 @@
 require "#{File.dirname(__FILE__)}/../lib/rack/promises"
+require "#{File.dirname(__FILE__)}/../lib/rack/promises/no_promise_call_error"
 
 describe Rack::Promises do
   before(:each) do
@@ -11,5 +12,12 @@ describe Rack::Promises do
   it "should respond to call" do
     instance = SomeRackClass.new
     instance.should respond_to(:call)
+  end
+
+  it "should raise an exception if call is called and pcall is not defined" do
+    instance = SomeRackClass.new
+    -> {
+      instance.call({})
+    }.should raise_error NoPromiseCallError
   end
 end
